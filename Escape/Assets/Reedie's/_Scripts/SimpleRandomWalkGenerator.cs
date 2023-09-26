2bb3eb75208818ddbca0a4f38296e4af60c6c376
Jack <jackjreed06@gmail.com>
2023-09-26 11:41:11 +0930

    Added Folders

---
+++ Escape/Assets/Reedie's/_Scripts/SimpleRandomWalkGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalkGenerator : AbstractGenerator
+{
+    [SerializeField]
+    private int iterations = 10;
+    [SerializeField]
+    public int walkLength = 10;
+    [SerializeField]
+    public bool startRandomlyEachIteration = true;
+
+    protected override void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        tilemapVisualizer.Clear();
+        tilemapVisualizer.PaintFloorTiles(floorPositions);
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk()
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+        for (int i = 0; i < iterations; i++)
+        {
+            var path = GenerationAlgorithms.SimpleRandomWalk(currentPosition, walkLength);
+            floorPositions.UnionWith(path);
+            if (startRandomlyEachIteration)
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+        }
+        return floorPositions;
+    }
+}
