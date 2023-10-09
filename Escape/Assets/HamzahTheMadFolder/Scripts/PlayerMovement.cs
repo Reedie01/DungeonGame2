0906838478a0dbe23d2452dc88f306041c534e76
LINKPC\xerot <xero.twitch1@gmail.com>
2023-10-10 10:14:23 +1030

    Revert "Got Animations working"
    This reverts commit 71e689a29d804585ef1f68237978b5727768e863.

--- Escape/Assets/HamzahTheMadFolder/Scripts/PlayerMovement.cs
+++ Escape/Assets/HamzahTheMadFolder/Scripts/PlayerMovement.cs
@@ -4,35 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Animator anim;
-
-    Vector2 movement;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.sqrMagnitude);
-
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
-        {
-            anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
-        }
-    }
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
     }
 }
